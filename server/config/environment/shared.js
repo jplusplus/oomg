10f950e5c81d8055a693011781dffa9ca1de469f
Pirhoo <hello@pirhoo.com>
2016-11-07 15:57:19 +0100

    Allign

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -1,8 +1,6 @@
 'use strict';
 
 exports = module.exports = {
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
   gameConfig: {
     answersByQuestion: 3,
     questionsByGame: 8,
@@ -11,9 +9,16 @@ exports = module.exports = {
     answerTypes: ['comparison_text', 'numerical_form', 'written_form']
   },
   formConfig: {
-    countries: [ 'Austria', 'Belgium', 'Czech Republic', 'France', 'Germany', 'Greece', 'Hungary', 'Italy', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Spain', 'Sweden', 'United Kingdom', 'Other EU', 'Other Non-EU' ],
     ages: [ 'Under 20', '20-30', '31-40', '41-50', '51-60', 'Over 60'],
-    occupations: [ 'Armed forces', 'Private sector professional', 'Civil servant', 'Computer scientist/developer', 'Journalist', 'Lawyer', 'Manual worker, craftsman/woman', 'Medical doctor, dentist, optician', 'Public relations', 'Teacher', 'University professor', 'Student', 'No occupation', 'Other' ]
+    countries: [ 'Austria', 'Belgium', 'Czech Republic', 'France', 'Germany',
+                 'Greece', 'Hungary', 'Italy', 'Netherlands', 'Poland',
+                 'Portugal', 'Romania', 'Spain', 'Sweden', 'United Kingdom',
+                 'Other EU', 'Other Non-EU' ],
+    occupations: [ 'Armed forces', 'Private sector professional',
+                   'Civil servant', 'Computer scientist/developer',
+                   'Journalist', 'Lawyer', 'Manual worker, craftsman/woman',
+                   'Medical doctor, dentist, optician', 'Public relations',
+                   'Teacher', 'University professor', 'Student',
+                   'No occupation', 'Other' ]
   }
 };
-
